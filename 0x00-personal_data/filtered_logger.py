3b1aafafa5276d66018a284e0a98404cf651d4ba
Madele <madele.theron@gmail.com>
2023-05-25 19:58:20 +0200

    fix pycodestyle issues

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Module"""
+"""Write a function called filter_datum that returns the log """
 import logging
 import re
 from typing import List
+
 
 def filter_datum(
         fields: List[str], redaction: str, message: str, seperator: str
@@ -10,7 +11,5 @@ def filter_datum(
     """Returns the log message obfuscated"""
     for field in fields:
         message = re.sub(f"{field}=.+?{seperator}",
-                        f"{field}={redaction}{seperator}", message)
+                         f"{field}={redaction}{seperator}", message)
     return message
-    
-    
