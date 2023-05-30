ecdcdf4a13ee50c4bb4e47f0100107e8a88ceeef
Madele <madele.theron@gmail.com>
2023-05-30 22:00:17 +0200

    task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,15 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+AUTH_TYPE = getenv('AUTH_TYPE')
+
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -46,6 +55,30 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+@app.before_request
+def before_request() -> str:
+    """Execute this before each each request
+
+    Returns:
+        str: or nothing
+    """
+    if auth is None:
+        return
+
+    expath = [
+        '/api/v1/status',
+        '/api/v1/status',
+        '/api/v1/forbidden'
+    ]
+
+    if not (auth.require_auth(request.path, expath)):
+        return
+
+    if (auth.require_auth(request)) is None:
+        abort(401)
+
+    if (auth.current_user(request)) is None:
+        abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
