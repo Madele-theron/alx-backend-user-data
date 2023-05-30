9a5cfc9d80b6a3c5f0de0869c317962317244734
Madele <madele.theron@gmail.com>
2023-05-30 22:21:59 +0200

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,5 +8,25 @@ from auth import Auth
 
 
 class BasicAuth(Auth):
-    """Manages API authentication
+    """Manages basic authentication
     """
+
+    def __init__(self):
+        """This is the constructor"""
+
+    def extract_base64_authorization_header(
+                                            self,
+                                            authorization_header: str) -> str:
+        """header extraction in base64
+
+        Args:
+            authorization_header (str): string in base64
+
+        Returns:
+            str: header in base64, else None
+        """
+        if authorization_header is None or\
+            isinstance(authorization_header, str) or\
+                authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[1]
