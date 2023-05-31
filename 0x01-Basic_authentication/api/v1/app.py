833449ce6f39c6c7fd69792cc34d8de055585024
Madele <madele.theron@gmail.com>
2023-05-31 22:13:38 +0200

    before requet fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -77,7 +77,7 @@ def before_request() -> str:
     if not (auth.require_auth(request.path, expath)):
         return
 
-    if (auth.require_auth(request)) is None:
+    if (auth.authorization_header(request)) is None:
         abort(401)
 
     if (auth.current_user(request)) is None:
