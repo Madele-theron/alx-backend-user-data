5b7cbf5e5187c868009f23513d6f935fd91aec9f
Madele <madele.theron@gmail.com>
2023-05-31 22:53:04 +0200

    task 7 & style errors

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -69,3 +69,51 @@ def extract_user_credentials(self,
         return None, None
     user_cred = decode_base64_authorization_header.split(":", 1)
     return user_cred[0], user_cred[1]
+
+
+def user_object_from_credentials(self, user_email: str,
+                                 user_pwd: str) -> TypeVar('User'):
+    """
+        Get user object from creds
+
+        Args:
+            user_email: Email
+            user_pwd: password
+
+        Return:
+            The user, else None
+    """
+    if user_email is None or not isinstance(user_email, str):
+        return None
+    if user_pwd is None or not isinstance(user_pwd, str):
+        return None
+    try:
+        users = User.search({'email': user_email})
+    except Exception:
+        return None
+
+    for user in users:
+        if user.is_valid_password(user_pwd):
+            return user
+
+
+def current_user(self, request=None) -> TypeVar('User'):
+    """
+        Get current user details
+
+        Args:
+            request: user
+
+        Return:
+            Detail of current user, else None
+    """
+    try:
+        header: str = self.authorization_header(request)
+        head64: str = self.extract_base64_authorization_header(header)
+        auth_decode: str = self.decode_base64_authorization_header(head64)
+        user_cred = self.extract_user_credentials(auth_decode)
+        user = self.user_object_from_credentials(user_cred[0],
+                                                 user_cred[1])
+        return user
+    except Exception:
+        return None
