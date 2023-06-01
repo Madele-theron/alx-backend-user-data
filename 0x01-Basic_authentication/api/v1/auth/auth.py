4bb12c42bec98a3fd56fb9bb60c22ee5a51f963d
Madele <madele.theron@gmail.com>
2023-06-01 22:33:15 +0200

    ,

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,7 @@ class Auth:
             if paths.endswith('*'):
                 if path.startswith(paths[:-1]):
                     return False
-            if path in excluded_paths:
+            if path == paths:
                 return False
         return True
 
