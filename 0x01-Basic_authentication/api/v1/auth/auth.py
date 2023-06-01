19942fdf0ffc17a4efcf736750b57b9416a3d8a8
Madele <madele.theron@gmail.com>
2023-06-01 22:37:22 +0200

    .

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,8 @@ from typing import List, TypeVar
 class Auth:
     """Manages API authentication
     """
+    def __init__(self):
+        """The construcotr"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication
