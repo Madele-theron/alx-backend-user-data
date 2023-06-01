fc87320e38df641c57c0fc421e3dd3613409f31d
Madele <madele.theron@gmail.com>
2023-06-01 22:30:49 +0200

    add back

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,7 @@ class Auth:
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
-        if path[-1] is not '/':
+        if not path.endswith('/'):
             path += '/'
 
         for paths in excluded_paths:
