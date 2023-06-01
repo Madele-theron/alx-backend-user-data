6b9aebf0f0088ab865d9229a38c51c95b7b9d993
Madele <madele.theron@gmail.com>
2023-06-01 22:29:48 +0200

    add constrcutr

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,8 @@ from typing import List, TypeVar
 class Auth:
     """Manages API authentication
     """
+    def __init__(self):
+        """This is the constructor"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication
