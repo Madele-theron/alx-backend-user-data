325178eb4c4f06e444ebd60283b7739a9e22a63e
Madele <madele.theron@gmail.com>
2023-06-06 22:39:42 +0200

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+"""
+Module DB
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
+
+
+class DB:
+    """ DB Class for the database """
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+            Method to create a new user
+
+            Args:
+                email: email of user
+                hashed_password: hashed password
+
+            Return:
+                User object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+
+        return user
