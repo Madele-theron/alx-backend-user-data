1bf8e813db3445d20c4f060b9a133d72578cd1ef
Madele <madele.theron@gmail.com>
2023-06-07 22:27:46 +0200

    .

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,7 +26,7 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-    
+
     def register_user(self, email: str, password: str) -> User:
         """Method to register a new user with auth
 
