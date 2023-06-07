f300b078a8c052b0ba890dd6f36129b8aba8217b
Madele <madele.theron@gmail.com>
2023-06-07 22:23:04 +0200

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,3 +18,30 @@ def _hash_password(password: str) -> str:
         str: hashed password
     """
     return bcrypt.hashpw(password=password.encode(), salt=bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method to register a new user with auth
+
+        Args:
+            email (str): user email
+            password (str): user password
+
+        Returns:
+            User: Registered User
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {user.email} already exists")
+        except NoResultFound:
+            pswd = _hash_password(password=password)
+            user = self._db.add_user(email, pswd)
+
+        return user
