ad5723a06b8a3cb85959454adf707ce7263ba8bb
Madele <madele.theron@gmail.com>
2023-06-07 21:36:26 +0200

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""Module for the Auth class
+"""
+from uuid import uuid4
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> str:
+    """Convert password to hashed password
+
+    Args:
+        password (str): password to be hashed
+
+    Returns:
+        str: hashed password
+    """
+    return bcrypt.hashpw(password=password.encode(), salt=bcrypt.gensalt())
