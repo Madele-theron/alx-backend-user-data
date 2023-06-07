098eb21f5bffbc45abab45aab1f0829912547675
Madele <madele.theron@gmail.com>
2023-06-07 20:57:14 +0200

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -43,3 +43,24 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Method to find a user using keyword arguments
+
+        Returns:
+            User: User that was searched for
+        """
+        if kwargs is None:
+            raise InvalidRequestError
+
+        for key in kwargs.keys():
+            if key not in User.__table__.columns.keys():
+                raise InvalidRequestError
+
+        user = self.__session.query(User).filter_by(
+                                                    **kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
