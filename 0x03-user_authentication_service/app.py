fd72c5d002e7be013e4518e5b568a896235e8cca
Madele <madele.theron@gmail.com>
2023-06-08 23:11:47 +0200

    task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,5 +72,18 @@ def logout() -> str:
     return redirect('/', code=302)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """Get user profile
+    """
+    session = request.cookies.get('session_id', None)
+    user = AUTH.get_user_from_session_id(session)
+
+    if session is None or user is None:
+        abort(403)
+
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
